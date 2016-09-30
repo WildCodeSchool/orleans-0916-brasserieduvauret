ee03370527271dbe3f2f3edc6c2f3b7a27a07872
PatrickS45 <tuko45@hotmail.fr>
2016-09-30 11:27:32 +0200

    Ajout de la thumbnail et du module facebook

---
+++ fil_actualite.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Fil actualite</title>
+    <link rel="stylesheet" href="/content.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+   
+ <!--début de fil d'actualité-->
+   
+          <!--module image-->
+           
+    <div class="image_une">
+        <div class="row">
+            <div class="col-xs-12 col-md-6">
+                <a href="#" class="thumbnail"> <img src="image/test_a_la_une.png" alt="La bière à la une"> </a>
+            </div>
+            
+            <!--module facebook-->
+            
+            <div class="col-xs-12 col-md-6" class="facebook">
+                     <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fbrasserieduvauret%2F%3Ffref%3Dts&tabs=timeline&width=438&height=444&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId=44112537848" width="444" height="438" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+            </div>
+        </div>
+    </div>
+    
+    
+    
+    
+    
+    // Fin de fil d'actualité 
+    
+</body>
+
+</html>
