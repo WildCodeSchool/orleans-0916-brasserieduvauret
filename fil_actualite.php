f9813e3a42a2eb008eafb877ca90546cb2bb9ff7
PatrickS45 <tuko45@hotmail.fr>
2016-09-30 16:45:18 +0200

    Centrage map et titre h2

--- fil_actualite.php
+++ fil_actualite.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Fil actualite</title>
-    <link rel="stylesheet" href="/content.css">
+    <link rel="stylesheet" href="content.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
