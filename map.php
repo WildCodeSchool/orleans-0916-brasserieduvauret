74021f2e42d638f296a16e59a849cf79d1063033
PatrickS45 <tuko45@hotmail.fr>
2016-09-30 16:46:29 +0200

    Centrage map et titre h2

---
+++ map.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="map.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>Map accueil</title>
+</head>
+<body>
+   
+   
+   <!---Debut Map -------->
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="map_accueil" ><div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+               <h2>Retrouvez tous nos points de ventes</h2>
+                <a href="#"><img src="image/test%20carte.png" class="img-responsive center-block" alt="Carte des points de vente"></a>
+        </div>
+            </div>
+        </div>
+    </div>
+    
+    
+   <!---fin Map -------->
+   
+    
+</body>
+</html>
