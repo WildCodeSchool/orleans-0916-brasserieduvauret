1a3d1474c3eb63b8d3c2f82af99971cf4be1261f
PatrickS45 <tuko45@hotmail.fr>
2016-09-30 11:55:05 +0200

    insertion titre h2

--- fil_actualite.php
+++ fil_actualite.php
@@ -13,31 +13,24 @@
 </head>
 
 <body>
-   
- <!--début de fil d'actualité-->
-   
-          <!--module image-->
-           
-    <div class="image_une">
-        <div class="row">
-            <div class="col-xs-12 col-md-6">
-                <a href="#" class="thumbnail"> <img src="image/test_a_la_une.png" alt="La bière à la une"> </a>
-            </div>
-            
-            <!--module facebook-->
-            
-            <div class="col-xs-12 col-md-6" class="facebook">
-                     <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fbrasserieduvauret%2F%3Ffref%3Dts&tabs=timeline&width=438&height=444&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId=44112537848" width="444" height="438" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+    <!--début de fil d'actualité-->
+    <!--module image-->
+    <div class="container-fluid">
+       
+        <div class="image_une">
+            <div class="row">
+              <h2>Bière à la Une</h2>
+                <div class="col-xs-12 col-md-6 col-sm-6 col-lg-6">
+                    <a href="#" class="thumbnail"> <img src="image/test_a_la_une.png" alt="La bière à la une"></a>
+                </div>
+                <!--module facebook-->
+                <div class="col-xs-12 col-md-6 col-sm-6 col-lg-6" class="facebook">
+                    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fbrasserieduvauret%2F%3Ffref%3Dts&tabs=timeline&width=438&height=444&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId=44112537848" width="444" height="438" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+                </div>
             </div>
         </div>
     </div>
-    
-    
-    
-    
-    
-    // Fin de fil d'actualité 
-    
+    <!-- Fin de fil d'actualité -->
 </body>
 
 </html>
