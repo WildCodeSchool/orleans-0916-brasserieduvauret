2564e1fb882e3c38c924ac11548a98316b92b88f
gthielon <gthielon@gmail.com>
2016-10-03 14:16:24 +0200

    Création jumbotron et icones animées

---
+++ jumbo.php
@@ -0,0 +1,72 @@
+<html lang="fr">
+    <head>
+        <title>jumbo</title>
+        <meta charset="utf-8">
+        <link type="text/css" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link type="text/css" rel="stylesheet" href="./css/style.css">
+        <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="js/bootstrap.js"></script>
+    </head>
+    <body>
+
+    <!--Bloc Jumbotron -->
+
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container-fluid">
+                <h1 class="display-3 slogan">"Phrase d'accroche bière"</h1>
+            </div>
+        </div>
+        <div class="rows icones">
+            <div class="col-lg-12 col-xs-2">
+                
+            </div>
+        </div>
+
+    <!-- Bloc icones animées -->
+
+        <div class="container-fluid row bloc1">
+            <a class="hover_color" href="#">
+            <div class="col-lg-offset-2 col-lg-2 col-md-offset-1 col-md-6 col-xs-offset-1 col-xs-12 icone1">
+                <div class="glyphicon glyphicon-info-sign center_icons">
+
+                </div>
+            </div>
+            </a>
+            <a class="hover_color" href="#">
+            <div class="col-lg-offset-2 col-lg-2 col-md-offset-1 col-md-6 col-xs-offset-1 col-xs-12 icone2">
+                <div class="glyphicon glyphicon-home center_icons">
+
+                </div>
+
+            </div>
+            </a>
+            <a class="hover_color" href="#">
+            <div class="col-lg-offset-2 col-lg-2 col-md-offset-1 col-md-6 col-xs-offset-1 col-xs-12 icone3">
+                <div class="glyphicon glyphicon-music center_icons">
+
+                </div>
+
+            </div>
+            </a>
+        </div>
+        <div class="container-fluid row bloc-text">
+            <div class="col-lg-offset-2 col-lg-2 col-md-offset-1 col-md-6 col-xs-offset-1 col-xs-12 text1">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quam est, porttitor at lectus vitae,
+                    pretium consequat diam. Cras nec diam vitae lectus varius lacinia eu a nisl. Proin tempor lobortis nibh non iaculis.
+                    Fusce tincidunt neque eu metus sagittis condimentum. Nunc vehicula tincidunt ante eget mattis.</p>
+            </div>
+            <div class="col-lg-offset-1 col-lg-2 col-md-offset-1 col-md-6 col-xs-offset-1 col-xs-12 text1">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quam est, porttitor at lectus vitae,
+                    pretium consequat diam. Cras nec diam vitae lectus varius lacinia eu a nisl. Proin tempor lobortis nibh non iaculis.
+                    Fusce tincidunt neque eu metus sagittis condimentum. Nunc vehicula tincidunt ante eget mattis.</p>
+            </div>
+            <div class="col-lg-offset-1 col-lg-2 col-md-offset-1 col-md-6 col-xs-offset-1 col-xs-12 text1">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quam est, porttitor at lectus vitae,
+                    pretium consequat diam. Cras nec diam vitae lectus varius lacinia eu a nisl. Proin tempor lobortis nibh non iaculis.
+                    Fusce tincidunt neque eu metus sagittis condimentum. Nunc vehicula tincidunt ante eget mattis.</p>
+            </div>
+        </div>
+    </body>
+
+</html>
