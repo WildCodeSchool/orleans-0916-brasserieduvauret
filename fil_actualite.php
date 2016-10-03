1609abc2ddc397a629fdd0949567c5d54f47afd6
PatrickS45 <tuko45@hotmail.fr>
2016-10-03 11:29:00 +0200

    Padding bottom h2 + correction generale du html

--- fil_actualite.php
+++ fil_actualite.php
@@ -4,28 +4,29 @@
 <head>
     <meta charset="UTF-8">
     <title>Fil actualite</title>
-    <link rel="stylesheet" href="/content.css">
+    <link rel="stylesheet" href="content.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
 </head>
 
 <body>
     <!--début de fil d'actualité-->
     <!--module image-->
+      <h2>Bière à la Une</h2>
     <div class="container-fluid">
        
         <div class="image_une">
             <div class="row">
-              <h2>Bière à la Une</h2>
-                <div class="col-xs-12 col-md-6 col-sm-6 col-lg-6">
-                    <a href="#" class="thumbnail"> <img src="image/test_a_la_une.png" alt="La bière à la une"></a>
+                <div class="col-lg-offset-3 col-lg-3 col-md-offset-3 col-md-3 col-sm-6 col-xs-12">
+                    <a href="#" class="thumbnail"> <img class="img-rounded center-block"  src="image/test_a_la_une.png" alt="La bière à la une"></a>
                 </div>
                 <!--module facebook-->
-                <div class="col-xs-12 col-md-6 col-sm-6 col-lg-6" class="facebook">
-                    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fbrasserieduvauret%2F%3Ffref%3Dts&tabs=timeline&width=438&height=444&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId=44112537848" width="444" height="438" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12" class="facebook">
+                    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fbrasserieduvauret%2F%3Ffref%3Dts&tabs=timeline&width=438&height=444&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId=44112537848" width="444" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
                 </div>
             </div>
         </div>
