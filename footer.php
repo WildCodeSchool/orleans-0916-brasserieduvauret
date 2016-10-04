28cc5596fec94b326d17736d1b3c5a02377c5098
Kim Nguyen <knguyen.wcs@gmail.com>
2016-10-04 14:03:16 +0200

    depot initial

---
+++ footer.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="fr">  
+<!-- Make sure the <html> tag is set to the .full CSS class. Change the background image in the full.css file. -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Brasserie du Vauret</title>
+    <link href="https://fonts.googleapis.com/css?family=Homemade+Apple" rel="stylesheet"> 
+  <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/full.css" rel="stylesheet">
+    <link href="css/half-slider.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/footer-distributed-with-address-and-phones.css">
+    <link rel="stylesheet" href="css/demo.css">
+	<link rel="stylesheet" href="css/footer-basic-centered.css">
+	<!-- Custom Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Arbutus+Slab" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Cinzel|Great+Vibes|Ovo|Playfair+Display+SC|Rufina" rel="stylesheet">
+
+
+<script src="https://use.fontawesome.com/4b73a13289.js"></script>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+    
+
+
+
+<!--footer-->
+<footer class="footer-distributed">
+            <div class="row top-buffer">
+    
+			<!-- <div class="footer-left"> -->
+                
+                    
+			<!--	<img src="http://img1.imagilive.com/0916/logo829.png" alt="image du logo" /> -->
+				<p class="footer-links">
+                    
+               
+                   <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12 bloccouleur">
+                    
+                    
+                    
+                   	<ul>
+                            <li><a href="http://http://www.brasserieduvauret.fr/accueil">Brasserie du Vauret</a></li>
+                            <li><a href="http://http://www.brasserieduvauret.fr/page2">Nos produits</a></li>
+                            <li><a href="http://http://www.brasserieduvauret.fr/page3">La Micro-brasserie</a></li>
+                            <li><a href="http://http://www.brasserieduvauret.fr/page4">Nos points de vente</a></li>
+                            <li><a href="http://http://www.brasserieduvauret.fr/page5">Actualités de la brasserie</a></li>
+                        </ul> 
+                    	
+                    
+             		</p>
+
+			<p class="footer-company-name">Brasserie du Vauret &copy; WildCodeSchool2016</p><br/>
+				
+			</div>
+
+                
+                
+    
+                
+		<!--	<div class="footer-center">  -->
+                
+                
+                
+                <div class="row top-buffer">
+                   <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12 bloccouleur">
+                
+                
+                
+					<div class="map-icons">
+					<!-- Trigger/Open The Modal -->
+					<i class="fa fa-map-marker" button id="myBtn"></i></button>
+					<!-- The Modal -->
+					<div id="myModal" class="modal">
+  					<!-- Modal content -->
+  					<div class="modal-content">
+  					<img src="http://img1.imagilive.com/0916/crop.png" alt="carte adresse wildshoes">
+  					<div class="map-link"><a href="http://www.openstreetmap.org/?mlat=47.89364&amp;mlon=1.89426#map=17/47.89364/1.89426&amp;layers=N">View Larger Map</a></div>
+					<span class="close">x</span>
+				 	</div>
+
+				</div>
+					<p><span>501 rue du Vauret</span> 45430 Mardié</p>
+				</div>
+				<div>
+					<i class="fa fa-phone"></i>
+					<p>07 83 76 08 49</p>
+				</div>
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p><a href="mailto:support@cwildshoes.com">contact.brasserie@free.fr</a></p>
+				</div>
+			</div>
+                
+                
+                
+                
+                
+    
+    
+    
+    
+    
+    
+    
+    
+                
+                
+		<!--	<div class="footer-right">  -->
+                <div class="row top-buffer">
+                   <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12 bloccouleur">
+                
+				<p class="footer-company-about">
+					<span>Newsletters</span>
+					Recevez notre newsletter en vous abonnant ici :
+				</p>
+
+
+
+				<form class="navbar-form navbar-left">
+
+        			<div class="form-group">
+          			<input type="text" class="form-control" placeholder="votre email">
+        			</div>
+        	<button type="submit" class="btn btn-default">Je valide</button>
+      		</form><br><br>
+
+
+
+
+
+
+
+				<div class="footer-icons">
+
+					<a href="#"><i class="fa fa-facebook"></i></a>
+					
+				</div>
+	</div>
+			</div>
+		</footer> <!--fin du footer-->
+    
+    
+    
+    <i class="fa fa-angle-up"></i>
+    
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
