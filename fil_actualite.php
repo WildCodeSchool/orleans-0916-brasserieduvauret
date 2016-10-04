dfb559814b7283ddb89d08e19d83f7695d1a89d9
PatrickS45 <tuko45@hotmail.fr>
2016-10-04 16:16:37 +0200

    maj responsive module facebook

--- fil_actualite.php
+++ fil_actualite.php
@@ -2,12 +2,16 @@
 <html lang="fr">
 
 <head>
+   
     <meta charset="UTF-8">
     <title>Fil actualite</title>
-    
+  
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <!-- jQuery library -->
+   
     <link rel="stylesheet" href="content.css">
+    
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -27,18 +31,21 @@
     <div class="container-fluid">
         <div class="image_une">
             <div class="row">
-                <div class="col-lg-offset-2 col-lg-3 col-md-offset-3 col-md-3 col-sm-offset-2 col-sm-3 colm-sm-3 col-xs-12">
+                <div class="col-lg-offset-2 col-lg-3 col-md-offset-3 col-md-3 col-sm-offset-1 col-sm-12 col-xs-12">
                     <a href="#"> <img class="img-responsive" src="image/test_a_la_une.png" alt="La bière à la une"></a>
                 </div>
-                
                 <!--module facebook-->
-                <div class="col-lg-offset-1 col-lg-3 col-md-offset-1 col-md-3  col-sm-offset-1 col-sm-3 col-xs-12" class="facebook">
-                    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fbrasserieduvauret%2F%3Ffref%3Dnf%23&tabs=timeline&width=438&height=444&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=true&appId=44112537848" width="438" height="444" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+                <div class="col-lg-offset-2 col-lg-5 col-md-offset-2 col-md-5 col-sm-12 col-xs-12" class="facebook">
+                   
+                                       <!--iframe debut-->
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fbrasserieduvauret%2F&tabs=timeline&width=500&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=44112537848" width="493" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+                        <!--iframe fin-->
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <!-- Fin de fil d'actualité -->
+        <!-- Fin de fil d'actualité -->
 </body>
 
 </html>
