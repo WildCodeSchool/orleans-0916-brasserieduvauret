bcdce4b9929e2198f8e3b33fdaa75f0696cb67d7
gthielon <gthielon@gmail.com>
2016-10-11 16:28:37 +0200

    maj2

--- pages/navbar.php
+++ pages/navbar.php
@@ -1,15 +1,8 @@
 <!--navbar-->
-
-
-
 <nav class="navbar navbar-default">
 
 <nav class="navbar navbar-default navbar-fixed-top">
   <div class="container-fluid babar">
-
-
-
-    <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
@@ -17,39 +10,20 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-
 <div class="logo">
       <a class="navbar-brand" href="#"><img class="img-responsive" src="img/logo.png"  alt="logo Brasserie" ></a></div>
     </div>
-
-
-
-
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-
-
-	
-
-
-	
         <li class="active liste_menu"><a href="#">Accueil <span class="sr-only">(current)</span></a></li>
-	       
-	<li class="liste_menu"><a href="#">Nos bières</a></li>
-	<li class="liste_menu"><a href="#">La Brasserie</a></li>
+        <li class="liste_menu"><a href="#">Nos bières</a></li>
+        <li class="liste_menu"><a href="#">La Brasserie</a></li>
       	<li class="liste_menu"><a href="#">Points de vente</a></li>
-	<li class="liste_menu"><a href="#">Actualités</a></li>
-	<li class="liste_menu"><a href="#">Contact</a></li>
-
-
-	</ul>
-
-
-
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
+        <li class="liste_menu"><a href="#">Actualités</a></li>
+        <li class="liste_menu"><a href="#">Contact</a></li>
+      </ul>
+    </div>
+  </div>
 </nav>
 
 		
