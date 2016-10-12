7218fdf3600c3235cceb759e2539f7d33fcbeee2
gthielon <gthielon@gmail.com>
2016-10-12 14:15:57 +0200

    Modif footer

--- pages/footer.php
+++ pages/footer.php
@@ -1,21 +1,26 @@
 <div class="container-fluid foot-back">
 	<div class="row">
 		<div class="col-lg-4 foot-bloc1">
-			<ul class="foot-list">
-				<a href="#"><li>Accueil</li></a>
-				<a href="#"><li>Brasserie</li></a>
-				<a href="#"><li>Nos Bières</li></a>
-				<a href="#"><li>Actualités</li></a>
-				<a href="#"><li>Nos points de vente</li></a>
-				<a href="#"><li>contact</li></a>
-			</ul>
+			<h2 class="foot-contact">Plan du site</h2>
+			<p class="foot-list foot-contact">
+			Accueil<br>
+			Nos bières<br>
+			La Brasserie<br>
+			Nos points de vente<br>
+			Actualités<br>
+			Contact
+			</p>
 		</div>
 		<div class="col-lg-4 foot-bloc2">
-			<h1 class="foot-contact">Contactez-nous</h1>
+			<h2 class="foot-contact">Contactez-nous</h2>
+			<p class="foot-contact">par téléphone : 07 83 76 08 49<br>mail : cyril.bourassin@gmail.com<br>Facebook</p>
 			<img class="foot-logofb" src="img/facebook.png" alt="Logo Facebook">
+
 		</div>
 		<div class="col-lg-4 foot-bloc3">
-			 	<div class="col-lg-6">
+			 	<div class="foot-contact">
+			 		<h2>Newsletter</h2>
+			 		<p>Recevez notre newsletter en vous abonnant ici :</p>
     				<div class="input-group foot-input">
       				<input type="text" class="form-control" placeholder="Newsletter">
       					<span class="input-group-btn"><button class="btn btn-primary" type="button">Envoyer</button></span>
@@ -23,5 +28,16 @@
   				</div>
 		</div>
 	</div>
+</div>
 	</div>
+</div>
+
+<div class="container-fluid foot-back">
+	<div class="row">
+		<div class="col-lg12">
+				<div class="foot-contact foot-bloc4">
+					<h3>Mentions légales</h3>
+					<p>lorem ipsum : Postremo ad id indignitatis est ventum, ut cum peregrini ob formidatam haut ita dudum alimentorum inopiam pellerentur ab urbe praecipites, sectatoribus disciplinarum liberalium inpendio paucis sine respiratione ulla extrusis, tenerentur minimarum adseclae veri, quique id simularunt ad tempus, et tria milia saltatricum ne interpellata quidem cum choris totidemque remanerent magistris.</p>
+				</div>
+			</div>
 </div>
