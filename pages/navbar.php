f9bfbbfd9bb4c8c060ed4602e7381910e4dd1aba
PatrickS45 <tuko45@hotmail.fr>
2016-10-12 10:26:01 +0200

    Maj Navbar bg blanc

--- pages/navbar.php
+++ pages/navbar.php
@@ -5,12 +5,12 @@
         });
     });
 </script>
-<div class="navbar navbar-inverse navbar-top">
+<div class="navbar navbar navbar-fixed">
     <div class="navbar-header">
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button> <a class="navbar-brand" href="#">Brasserie du Vauret</a> </div>
     <div class="navbar-collapse collapse">
         <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Accueil</a></li>
+            <li role="presentation" class="active"><a href="#">Accueil</a></li>
             <li class="active"><a href="#">Les bières</a></li>
             <li class="active"><a href="#">La brasserie</a></li>
             <li class="active"><a href="#">Points de vente</a></li>
