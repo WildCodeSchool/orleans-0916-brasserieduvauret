78a3b110b486b4a2e3f8cd679a20620ca6902beb
PatrickS45 <tuko45@hotmail.fr>
2016-10-12 10:03:41 +0200

    maj de la navbar

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
     <html lang="fr">
         <head>
             <title>La Brasserie du Vauret</title>
-            <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+              <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
             <link rel="stylesheet" type="text/css" href="css/jumbo.css">
             <link rel="stylesheet" type="text/css" href="css/3ronds.css">
             <link rel="stylesheet" type="text/css" href="css/formulaire.css">
@@ -10,13 +12,11 @@
             <link rel="stylesheet" type="text/css" href="css/stylenavbar.css">
             <link href="https://fonts.googleapis.com/css?family=Spicy+Rice" rel="stylesheet"> 
             <link rel="stylesheet" type="text/css" href="css/fonts.css">
-
-
-
-
             <script src="js/bootstrap.min.js"></script>
         </head>
         <body>
+           
+           
             <div class="navbar1">
                 <?php
                 include 'pages/navbar.php';
--- pages/navbar.php
+++ pages/navbar.php
@@ -1,60 +1,21 @@
-<!--navbar-->
-
-
-
-<nav class="navbar navbar-default">
-
-<nav class="navbar navbar-default navbar-fixed-top">
-  <div class="container-fluid babar">
-
-
-
-    <!-- Brand and toggle get grouped for better mobile display -->
+<script>
+    $(document).ready(function () {
+        $(".navbar-toggle").on("click", function () {
+            $(this).toggleClass("active");
+        });
+    });
+</script>
+<div class="navbar navbar-inverse navbar-top">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-
-<div class="logo">
-      <a class="navbar-brand" href="#"><img class="img-responsive" src="img/logo.png"  alt="logo Brasserie" ></a></div>
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button> <a class="navbar-brand" href="#">Brasserie du Vauret</a> </div>
+    <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Accueil</a></li>
+            <li class="active"><a href="#">Les bières</a></li>
+            <li class="active"><a href="#">La brasserie</a></li>
+            <li class="active"><a href="#">Points de vente</a></li>
+            <li class="active"><a href="#">Actualités</a></li>
+            <li class="active"><a href="#">Contact</a></li>
+        </ul>
     </div>
-
-
-
-
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-
-
-	
-
-
-	
-        <li class="active liste_menu"><a href="#">Accueil <span class="sr-only">(current)</span></a></li>
-	       
-	<li class="liste_menu"><a href="#">Nos bières</a></li>
-	<li class="liste_menu"><a href="#">La Brasserie</a></li>
-      	<li class="liste_menu"><a href="#">Points de vente</a></li>
-	<li class="liste_menu"><a href="#">Actualités</a></li>
-	<li class="liste_menu"><a href="#">Contact</a></li>
-
-
-	</ul>
-
-
-
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-
-		
-
-
-
-		 <!--fin du navbar-->
- 
+</div>
