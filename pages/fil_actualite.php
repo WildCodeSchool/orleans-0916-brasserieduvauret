2b1bb8929ebca918bbce006b81116789cd64095e
PatrickS45 <tuko45@hotmail.fr>
2016-10-13 19:30:33 +0200

    Maj du fil actu

--- pages/fil_actualite.php
+++ pages/fil_actualite.php
@@ -1,39 +1,20 @@
 <div class="container-fluid">
+   
     <div class="row">
-        <div class="col-lg-offset-2 col-lg-3 col-md-offset-3 col-md-6 col-sm-12 col-xs-12">
-           <h2> Bière à la Une</h2>
+       
+        <div class="col-lg-offset-2 col-lg-3 col-md-offset-3 col-md-6 col-sm-offset-3 col-sm-6 col-xs-12">
+            <h2> Bière à la Une</h2>
+            <a href="#"> <img class="img-responsive" src="image/test_a_la_une.png" alt="La bière à la une"></a>
         </div>
-    
-        <div class="col-lg-offset-2 col-lg-3 hidden-md hidden-sm hidden-xs">
-            <h2> Suivez-nous !</h2> 
+     
+        <div class="col-lg-offset-2 col-lg-3 col-md-offset-3 col-md-6 col-sm-offset-3 col-sm-6 col-xs-12" class="facebook">
+            <h2> Suivez-nous !</h2>
+            <!--iframe debut-->
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fbrasserieduvauret%2F&tabs=timeline&width=500&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=44112537848" width="493" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+                <!--iframe fin-->
+            </div>
         </div>
+       
     </div>
 </div>
-
-<div class="container-fluid">
-    <div class="image_une">
-        <div class="row">
-           <div class="col-lg-offset-2 col-lg-3 col-md-offset-4 col-md-6 col-sm-offset-3 col-sm-12 col-xs-12">
-                <a href="#"> <img class="img-responsive" src="image/test_a_la_une.png" alt="La bière à la une"></a>
-            </div>
-            
-            
-         
-            <!--module facebook-->
-             <div class="hidden-lg col-md-12 col-sm-12 col-xs-12">
-            <h2> Suivez-nous !</h2> 
-            </div>
-            
-            <div class="col-lg-offset-2 col-lg-5 col-md-offset-4 col-md-6 col-sm-offset-3 col-sm-9 col-xs-12" class="facebook">
-                                   
-                                   <!--iframe debut-->
-                <div class="embed-responsive embed-responsive-16by9">
-                    <iframe class="embed-responsive" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fbrasserieduvauret%2F&tabs=timeline&width=500&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=44112537848" width="493" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
-                    <!--iframe fin-->
-                </div>
-               
-            </div>
-        </div>
-    </div>
-</div>
-    <!-- Fin de fil d'actualité -->
