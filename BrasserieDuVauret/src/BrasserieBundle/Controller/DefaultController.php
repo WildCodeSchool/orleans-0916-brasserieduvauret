b4c40cb171bd8a8eb5875432d968dc661718dbab
PatrickS45 <tuko45@hotmail.fr>
2016-10-18 15:05:08 +0200

    Maj BrasserieBundle

---
+++ BrasserieDuVauret/src/BrasserieBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace BrasserieBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('BrasserieBundle:Default:index.html.twig');
+    }
+}
